8240d9b3262bb158ca121afb05178300361e4a99
JatinKharbanda21104077 <97511139+JatinKharbanda21104077@users.noreply.github.com>
2022-03-03 00:33:16 +0530

    Assignment 4-21104077
    Assignment 4

---
+++ Assignment4(output)_21104077.py
@@ -0,0 +1,66 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====== RESTART: E:/PEC/Assignments/computing/ass4/Assignment4_21104077.py ======
+Solution to Problem 1
+Move Disk  1  from rod  A  to rod  B
+Move Disk  2  from rod  A  to rod  B
+Move Disk  1  from rod  C  to rod  A
+Move Disk  3  from rod  A  to rod  B
+Move Disk  1  from rod  C  to rod  A
+Move Disk  2  from rod  C  to rod  A
+Move Disk  1  from rod  B  to rod  C
+
+
+Solution to Problem 2
+Enter the no. of rows in Pascal's Triangle: 10
+By Recursion Procedure:
+                  1   
+                1   1   
+              1   2   1   
+            1   3   3   1   
+          1   4   6   4   1   
+        1   5   10   10   5   1   
+      1   6   15   20   15   6   1   
+    1   7   21   35   35   21   7   1   
+  1   8   28   56   70   56   28   8   1   
+1   9   36   84   126   126   84   36   9   1   
+
+
+By Iterative Method Procedure:
+           1 
+          1 1 
+         1 2 1 
+        1 3 3 1 
+       1 4 6 4 1 
+      1 5 10 10 5 1 
+     1 6 15 20 15 6 1 
+    1 7 21 35 35 21 7 1 
+   1 8 28 56 70 56 28 8 1 
+  1 9 36 84 126 126 84 36 9 1 
+Solution to Problem 3
+Enter 2 numbers: 5 15
+a. Checking if the quotient and remainder is a callable value or not.
+False
+False
+(b) The quotient is zero
+(c) Filtered out numbers that are greater than 4 are : [9, 10, 5, 10, 6, 11]
+(d) Set: {5, 6, 9, 10, 11}
+(e) Immutable set: frozenset({5, 6, 9, 10, 11})
+(f) Hash value of the max value from the above set: 11
+Solution to Problem 4
+The name of the student is Jatin and his/her SID is 21104077.
+The name of the student is Chiranjeev and his/her SID is 21104087.
+Object destroyed
+
+
+Solution to Problem 5:
+part-a
+The Updated Salary Of Mehak Is 70000 
+part-b
+Deleting record of employee Viren
+
+
+George's word: arc
+Barbie's word: car
+true friendship
